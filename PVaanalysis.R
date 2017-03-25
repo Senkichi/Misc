bdc8c73d854cdb3189568b5731c606fa8697c541
Senkichi <senkichi92@gmail.com>
2017-03-25 12:10:05 -0600

    Files

--- PVaanalysis.R
+++ PVaanalysis.R
@@ -1,2 +1,8 @@
 species.code <- 3490
 sdfsfs
+setwd("~/GitHub/Misc")
+##Sam's paths
+NM <- read.csv("D:/Programs/GitHub/Misc/NMexico/NMexico.csv")
+UT <- read.csv("D:/Programs/GitHub/Misc/Utah/Utah.csv")
+CO <- read.csv("D:/Programs/GitHub/Misc/Colorad/Colorad.csv")
+Az <- read.csv("D:/Programs/GitHub/Misc/Arizona/Arizona.csv")
